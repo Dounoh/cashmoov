6ec20d535eb65515169218ae17006a38b28bfb5f
Dounoh <fode.dounoh.5@gmail.com>
2026-02-04 17:38:38 +0000

    first_version_online_deploye

--- config/setting_prod.py
+++ config/setting_prod.py
@@ -1,7 +1,8 @@
 from config.settings import *
 
-
-CSRF_TRUSTED_ORIGINS = ["https://api.mondomaine.com"]
+USE_X_FORWARDED_HOST = True
+SECURE_SSL_REDIRECT = False
+CSRF_TRUSTED_ORIGINS = ["https://dev-dounoh.xyz"]
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 CI=False
 DEBUG=False
