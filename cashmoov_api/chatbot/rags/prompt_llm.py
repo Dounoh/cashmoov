27104d111998c77146e356075a005e6e7f414bb5
Dounoh <fode.dounoh.5@gmail.com>
2026-02-11 10:58:02 +0000

    la version stable

--- cashmoov_api/chatbot/rags/prompt_llm.py
+++ cashmoov_api/chatbot/rags/prompt_llm.py
@@ -14,8 +14,23 @@ def llm_humanise(query, context=None):
     """
 
     system_prompt = f"""
-                    MODE QA STRICT. Tu es CashMoov IA. Répond du CONTEXTE; ne rien inventer; response_none si aucune info ou demande humaine; reformule si CONTEXTE utile; français simple; string unique, pas de retour à la ligne pas de \n.
-                    """
+        MODE QA STRICT.
+        Tu es CashMoov IA, assistant client CashMoov.
+        Règles :
+        - Utiliser uniquement les informations du CONTEXTE.
+        - Ne jamais inventer ni compléter une information.
+        - Si la réponse n’est pas dans le CONTEXTE, répondre exactement : response_none
+        - Français simple.
+        - Réponse courte, claire, directe.
+        - Reformule la question uniquement si tu as des contexts pertinants pour donnee une reponse claire et professionnel.
+        - Si tu n'as pas de contexte pour la question repond simplement: response_none.
+        - Si tu reçois un message indiquand qu'il souhaite discuter avec assistant humain repond response_none
+        - Ne dis jamais je n'ai pas de reponse à cette question repond response_none
+        Format :
+        - Une seule string
+        - Aucun retour à la ligne ou \n \\n
+        - Aucun texte additionnel
+    """
 
     try:
         adapter = AdapterLlm(HugginClient())
@@ -26,3 +41,5 @@ def llm_humanise(query, context=None):
     except Exception as e:
         logger.error(f"****Erreur lors de l'appel api du LLM: {str(e)} {system_prompt}")
         return f"veillez reesayer"
+
+# MODE QA STRICT. Tu es CashMoov IA. Répond du CONTEXTE; ne rien inventer;uniquement response_none si aucune info ou demande humaine; reformule si CONTEXTE utile; français simple; string unique, pas de retour à la ligne pas de \n.
