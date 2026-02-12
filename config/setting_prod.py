813ed87fff729c4b1489f6033ac18acda2486980
Dounoh <fode.dounoh.5@gmail.com>
2026-02-12 00:41:56 +0000

    resolution du probleme de cors

--- config/setting_prod.py
+++ config/setting_prod.py
@@ -8,7 +8,7 @@ CI=False
 DEBUG=False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://cash-moov.vercel.app/",
+    "https://cash-moov.vercel.app",
 ]
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
