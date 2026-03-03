ba2d79ae3290c6aac8d08f7862f223574320429e
Dounoh <fode.dounoh.5@gmail.com>
2026-03-03 11:27:46 +0000

    resolution du probleme de cors

--- config/setting_prod.py
+++ config/setting_prod.py
@@ -2,13 +2,13 @@ from config.settings import *
 
 USE_X_FORWARDED_HOST = True
 SECURE_SSL_REDIRECT = False
-CSRF_TRUSTED_ORIGINS = ["https://dev-dounoh.xyz"]
+CSRF_TRUSTED_ORIGINS = ["https://dev-dounoh.xyz","https://api.dev-dounoh.xyz"]
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 CI=False
 DEBUG=False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://localhost:9000",
+    "https://dev-dounoh.xyz",
     "https://cash-moov.vercel.app",
 ]
 # CORS_ALLOW_ALL_ORIGINS = True
