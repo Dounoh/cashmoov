2628697b70a8d3b3729fe741161aca881c271b41
Dounoh <fode.dounoh.5@gmail.com>
2026-03-06 16:58:51 +0000

    debug1

--- config/setting_prod.py
+++ config/setting_prod.py
@@ -2,13 +2,15 @@ from config.settings import *
 
 USE_X_FORWARDED_HOST = True
 SECURE_SSL_REDIRECT = False
-CSRF_TRUSTED_ORIGINS = ["https://dev-dounoh.xyz","https://api.dev-dounoh.xyz"]
+CSRF_TRUSTED_ORIGINS = ["https://cashmoov.net","https://api.cashmoov.net","https://www.cashmoov.net"]
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 CI=False
 # DEBUG=False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://dev-dounoh.xyz",
+    "https://cashmoov.net",
+    "https://api.cashmoov.net",
+    "https://www.cashmoov.net",
     "https://cash-moov.vercel.app",
 ]
 # CORS_ALLOW_ALL_ORIGINS = True
